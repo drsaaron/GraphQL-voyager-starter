be3dc785fb8127f1bb9180648a0934f0dfdb12b9
Scott Aaron <dr_saaron@yahoo.com>
2025-11-03 15:11:35 -0600

    version update

--- src/test/java/com/blazartech/graphql/voyager/starter/VoyagerControllerTest.java
+++ src/test/java/com/blazartech/graphql/voyager/starter/VoyagerControllerTest.java
@@ -15,7 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;

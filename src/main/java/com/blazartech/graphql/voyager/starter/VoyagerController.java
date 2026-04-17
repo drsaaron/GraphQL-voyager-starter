55342bc3d003215a62c54c57ee6704a675110144
Scott Aaron <dr_saaron@yahoo.com>
2026-04-17 10:39:54 -0500

    version update

--- src/main/java/com/blazartech/graphql/voyager/starter/VoyagerController.java
+++ src/main/java/com/blazartech/graphql/voyager/starter/VoyagerController.java
@@ -5,6 +5,8 @@
  */
 package com.blazartech.graphql.voyager.starter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,11 +20,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VoyagerController {
     
+    private static final Logger logger = LoggerFactory.getLogger(VoyagerController.class);
+    
     @Value("${graphql.servlet.mapping}")
     private String graphQLMapping;
     
     @GetMapping(value = "${graphql.root}/voyager")
     public String getServerPath(Model model) {
+        logger.info("adding voyager path under {}", graphQLMapping);
         model.addAttribute("graphQLMapping", graphQLMapping);
         return "voyager";
     }
